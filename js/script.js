e4d6624faeda4358e0740d6b7aa4c241e546cd11
emanuelefarelli <emaaliens888@gmail.com>
2023-05-29 15:36:53 +0200

    Added team objects array

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const team = [
+    {
+        name: 'Wayne Barnett',     
+        role: 'founder & CEO',      
+        badge: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        name: 'Angela Caroll',     
+        role: 'Chief Editor',      
+        badge: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+        name: 'Walter Gordon',     
+        role: 'Office Manager',      
+        badge: 'walter-gordon-office-manager.jpg',
+    },
+    {
+        name: 'Angela Lopez',     
+        role: 'Social Media Manager',      
+        badge: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        name: 'Scott Estrada',     
+        role: 'Developer',      
+        badge: 'scott-estrada-developer.jpg',
+    },
+    {
+        name: 'Barbara Ramos',     
+        role: 'Graphic Designer',      
+        badge: 'barbara-ramos-graphic-designer.jpg',
+    },
+
+];
