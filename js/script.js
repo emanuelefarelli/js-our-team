6e84c90a9efb43f99f9a88c1fda7efc08e3d2dda
emanuelefarelli <emaaliens888@gmail.com>
2023-05-29 16:26:09 +0200

    Added information to dom in cards with images

--- js/script.js
+++ js/script.js
@@ -32,10 +32,28 @@ const team = [
 
 ];
 
+const myMain = document.querySelector('main');
+let teamList = document.createElement('ul');
+
 
 for(let i = 0 ; i < team.length ; i++){
+    const teamMember = document.createElement('li');
     const name = team[i].name;
     const role = team[i].role;
     const badge = team[i].badge;
+    teamMember.innerHTML = `
+        <div>
+            <img src="img/${badge}" alt="member-photo">
+            <p>
+                Nome: ${name}
+            </p>
+            <p>
+                Ruolo: ${role}
+            </p>
+        </div>
+    `;
+    teamList.append(teamMember);
     console.log(`membro: ${i} nome: ${name} ruolo: ${role} img: ${badge}`);
 }
+
+myMain.append(teamList);
